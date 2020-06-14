4558064467ce693fde4b1c5182113617f29e7d93
Hailey Ringier <haileyringier@outlook.com>
2020-06-14 21:45:30 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,12 +1,12 @@
 def count_strings(array)
-  array.count do | strings |
+  array.count do |strings|
     array.class == String
   end
 end
 
 
 def count_empty_strings(array)
-  array.count do | strings |
+  array.count do |strings|
     array.class == String && element == ""
   end
 end
